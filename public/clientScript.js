22d053a2148c83b277bde42131b4c89dd9b594b7
ags1773 <amogh1773@gmail.com>
2018-08-01 14:39:53 +0530

    created SignallingServer, accepting 'join' and sending 'addPeer' events

--- public/clientScript.js
+++ public/clientScript.js
@@ -1,19 +1,73 @@
 // **** CONFIG ****
 const SIGNALING_SERVER = 'http://localhost:3000'
+const USE_AUDIO = true
+const USE_VIDEO = true
+
+// **** Global ****
+// let localMediaStream = null
 
 const init = () => {
-  let socket = io(SIGNALING_SERVER)
+  // 1) establish socket connection with server
+  // 2) set up local media
+  // 3) emit 'join' event
+
+  let socket = io(SIGNALING_SERVER) // establishes socket connection
 
   socket.on('connect', () => {
     console.log('Connection to signaling server estabilished')
+    setupLocalMedia(function () {
+      socket.emit('join')
+    })
   })
 
-  document.querySelector('button').addEventListener('click', () => {
-    socket.emit('message', document.querySelector('input').value)
-    document.querySelector('input').value = ''
+  socket.on('addPeer', (peer) => {
+    console.log('add this peer => ', peer.peer_id)
   })
 
   socket.on('disconnect', () => {
     console.log('disconnected')
   })
+}
+
+// *********************** //
+// ** Local media stuff ** //
+// ***********************//
+
+function setupLocalMedia (callback) {
+  // Ask user for permission to use the computers microphone and/or camera,
+  // if .getUserMedia() is present, create a <video> element, attach it to <body>
+  // play webcam stream in it
+
+  const hasGetUserMedia = function () {
+    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) // !! (not twice) coerces object to boolean
+  }
+
+  const handleSuccess = (stream) => {
+    // create <video> element, display webcam stream in it
+    console.log('Got permission for camera and/or mic')
+
+    const video = document.createElement('video')
+    video.setAttribute('autoplay', '')
+    video.setAttribute('muted', 'true') // mute by default
+    video.setAttribute('controls', '')
+    document.body.appendChild(video)
+    video.srcObject = stream
+
+    if (callback) callback()
+  }
+
+  const handleError = (error) => {
+    if (error.name === 'PermissionDeniedError') {
+      console.log('Permissions have not been granted to use your camera and microphone')
+    } else console.log(`getUserMedia error: ${error.name}`, error)
+  }
+
+  if (hasGetUserMedia()) {
+    navigator.mediaDevices
+      .getUserMedia({audio: USE_AUDIO, video: USE_VIDEO})
+      .then(handleSuccess)
+      .catch(handleError)
+  } else {
+    console.error('getUserMedia() is not supported for your browser')
+  }
 }
--- server.js
+++ server.js
@@ -7,14 +7,32 @@ const io = socket(server)
 
 app.use(express.static('public'))
 
+const sockets = {}
+
 io.on('connection', function (socket) {
   console.log('Socket connection estabilished!')
 
-  socket.on('message', (msg) => {
-    console.log('Message recieved ', msg)
+  // on connection, add the socket to the 'sockets' obj
+  sockets[socket.id] = socket
+  console.log(`${socket.id} connection accepted`)
+
+  socket.on('join', () => {
+    // on getting a 'join', send out an 'addPeer' event with the socket ID of the other guy
+    console.log('Join event recieved')
+    console.log('sockets available', Object.keys(sockets))
+
+    if (Object.keys(sockets).length === 2) {
+      // broadcast 'addPeer', in it send own socket ID to other peer
+      // emit 'addPeer', in it send other peer's socket ID to self
+      socket.broadcast.emit('addPeer', {peer_id: socket.id})
+      socket.emit('addPeer', {
+        peer_id: Object.keys(sockets).filter((id) => id !== socket.id)[0]
+      })
+    }
   })
 
   socket.on('disconnect', () => {
     console.log('Disconnected')
+    // MAYBE?? remove socket entry from 'sockets' obj
   })
 })
