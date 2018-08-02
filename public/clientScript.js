ab1cfd31cdba5115d97015d9daf3948357bbe812
ags1773 <amogh1773@gmail.com>
2018-08-02 08:56:51 +0530

    (broken, WIP) one peer now sending offer to other peer

--- public/clientScript.js
+++ public/clientScript.js
@@ -2,16 +2,21 @@
 const SIGNALING_SERVER = 'http://localhost:3000'
 const USE_AUDIO = true
 const USE_VIDEO = true
+const ICE_SERVERS = [
+  {
+    urls: ['stun:stun.l.google.com:19302']
+  }
+]
 
 // **** Global ****
 // let localMediaStream = null
+let socket
+let localMediaStream
+let peer = {}
+let peerMediaElement = {}
 
 const init = () => {
-  // 1) establish socket connection with server
-  // 2) set up local media
-  // 3) emit 'join' event
-
-  let socket = io(SIGNALING_SERVER) // establishes socket connection
+  socket = io(SIGNALING_SERVER) // establishes socket connection
 
   socket.on('connect', () => {
     console.log('Connection to signaling server estabilished')
@@ -20,8 +25,68 @@ const init = () => {
     })
   })
 
-  socket.on('addPeer', (peer) => {
-    console.log('add this peer => ', peer.peer_id)
+  socket.on('addPeer', (config) => {
+    console.log('SignallingServer says add this peer => ', config)
+    // const peerId = peer.peer_id
+    const peerConnection = new RTCPeerConnection(
+      {'iceServers': ICE_SERVERS}
+    )
+    // peerConnection.addStream(localMediaStream)
+    peerConnection.addTrack(null, localMediaStream)
+    peer[config.peer_id] = peerConnection
+
+    peerConnection.onicecandidate = (event) => {
+      if (event.candidate) {
+        socket.emit('relayICECandidate', {
+          'peer_id': config.peer_id,
+          'ice_candidate': {
+            'sdpMLineIndex': event.candidate.sdpMLineIndex,
+            'candidate': event.candidate.candidate
+          }
+        })
+      }
+    }
+
+    peerConnection.ontrack = (event) => {
+      console.log('ontrack (old: onAddStream) event')
+      const video = document.createElement('video')
+      video.setAttribute('autoplay', '')
+      video.setAttribute('muted', 'true') // mute by default
+      video.setAttribute('controls', '')
+      peerMediaElement[config.peer_id] = video
+      document.body.appendChild(video)
+      video.srcObject = event.stream
+    }
+
+    if (config.should_create_offer) {
+      console.log('Creating RTC offer to ', config.peer_id)
+      peerConnection.createOffer()
+        .then(localDescription => {
+          console.log('Local offer description is: ', localDescription)
+          peerConnection.setLocalDescription(localDescription)
+            .then(() => {
+              // socket.emit('relaySessionDescription',
+              //   {
+              //     'peer_id': config.peer_id,
+              //     'session_description': localDescription
+              //   }
+              // )
+              console.log('Offer setLocalDescription succeeded')
+            })
+            .catch(err => console.error('Offer setLocalDescription failed!', err))
+        })
+        .catch(error => {
+          console.error('Error sending offer: ', error)
+        })
+    }
+  })
+
+  // Peers exchange session descriptions which contains information about their audio / video settings etc.
+  // and that sort of stuff. First, the 'offerer' sends a description to the 'answerer' (with type "offer"),
+  // then the answerer sends one back (with type "answer").
+
+  socket.on('sessionDescription', function (config) {
+
   })
 
   socket.on('disconnect', () => {
@@ -45,6 +110,8 @@ function setupLocalMedia (callback) {
   const handleSuccess = (stream) => {
     // create <video> element, display webcam stream in it
     console.log('Got permission for camera and/or mic')
+
+    localMediaStream = stream
 
     const video = document.createElement('video')
     video.setAttribute('autoplay', '')
--- server.js
+++ server.js
@@ -19,14 +19,15 @@ io.on('connection', function (socket) {
   socket.on('join', () => {
     // on getting a 'join', send out an 'addPeer' event with the socket ID of the other guy
     console.log('Join event recieved')
-    console.log('sockets available', Object.keys(sockets))
 
-    if (Object.keys(sockets).length === 2) {
-      // broadcast 'addPeer', in it send own socket ID to other peer
-      // emit 'addPeer', in it send other peer's socket ID to self
-      socket.broadcast.emit('addPeer', {peer_id: socket.id})
+    if (Object.keys(sockets).length === 2) { // both people connected
+      // broadcast 'addPeer' for other guy to recieve, in it send own socket ID
+      // emit 'addPeer' for own client to recieve, in it send other peer's socket ID
+      // so now, own client has others socket ID, and other client has my socket ID
+      socket.broadcast.emit('addPeer', {peer_id: socket.id, should_create_offer: false})
       socket.emit('addPeer', {
-        peer_id: Object.keys(sockets).filter((id) => id !== socket.id)[0]
+        peer_id: Object.keys(sockets).filter((id) => id !== socket.id)[0],
+        should_create_offer: true
       })
     }
   })
